e5fba2683c1c86200a9fce4df2d2a370b4bb7a18
shubhamtambere <shubham.tambere@gmail.com>
2019-01-30 00:51:25 +0530

    added static files. Version1 up

--- blog/settings.py
+++ blog/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/shubham/projects/django/src/blog/blogapp/static/blogapp/'
+STATICFILES_DIRS = ['/home/shubham/projects/django/src/blog/blogapp/static/blogapp/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
