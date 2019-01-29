9061145d05ee81439aae81ae16f18dcfd8d20d1c
shubhamtambere <shubham.tambere@gmail.com>
2019-01-29 23:15:30 +0530

    third commit

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Article
 # Create your views here.
 def home_view(*args, **kwargs):
     return(HttpResponse('Home page'))
 
 def article_list(request):
-    return render(request, 'blogapp/article_list.html', {})
+    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blogapp/article_list.html', {'articles':articles})
